a43fdd2593c440984c5d1c15075228e56e74f6cd
Giulianno Sande Monteiro <91056214+Giusande@users.noreply.github.com>
2023-11-18 21:33:00 -0300

    Add files via upload

---
+++ perfil/script.js
@@ -0,0 +1,27 @@
+if(localStorage.getItem('token') == null){
+    window.location.href = '../login/'
+}
+
+var x = document.querySelector('#x');
+var menu = document.querySelector('#menu');
+var navbar = document.querySelector('.mobile');
+
+menu.onclick = () => {
+    navbar.classList.add("active");
+};
+
+x.onclick = () => {
+    navbar.classList.remove("active");
+};
+
+let nome = document.querySelector('#nome')
+let email = document.querySelector('#email')
+let userLogado = JSON.parse(localStorage.getItem('userLogado'))
+
+nome.innerHTML = 'Olá, ' + userLogado.nome;
+
+email.innerHTML = userLogado.user;
+
+function logout(){
+  window.location.href = '../login/'
+}
