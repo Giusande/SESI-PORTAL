ebeb7205d92993a53b6454e405c6855550801973
Giulianno Sande Monteiro <91056214+Giusande@users.noreply.github.com>
2023-11-19 18:40:59 -0300

    Update script.js

--- perfil/script.js
+++ perfil/script.js
@@ -1,6 +1,12 @@
+let userLogado = JSON.parse(localStorage.getItem('userLogado'))
+
 if(localStorage.getItem('token') == null){
-    window.location.href = '../login/'
+  window.location.href = "./login/index.html"
 }
+
+let logado = document.querySelector('#logado')
+
+logado.innerHTML = 'Olá, ' + userLogado.nome;
 
 var x = document.querySelector('#x');
 var menu = document.querySelector('#menu');
@@ -14,16 +20,9 @@ x.onclick = () => {
     navbar.classList.remove("active");
 };
 
-let nome = document.querySelector('#nome')
-let email = document.querySelector('#email')
-let userLogado = JSON.parse(localStorage.getItem('userLogado'))
-
-nome.innerHTML = 'Olá, ' + userLogado.nome;
-
-email.innerHTML = userLogado.user;
-
 function logout(){
-  window.location.href = '../login/'
+  localStorage.removeItem('token');
+  window.location.href = "https://giusande.github.io/SESI-PORTAL/"
 }
 
 var perfilog = document.getElementById("perfil-log");
